265c41438e298875e9f599c8414f8488a15f8db6
David Emmanuel <eclefzy@gmail.com>
2025-09-01 10:52:11 +0100

    the assets has been added and the navbar has been styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faHouse} from '@fortawesome/free-solid-svg-icons'
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
   return (
     <>
       <h3><FontAwesomeIcon icon="fa-solid fa-dog" /><FontAwesomeIcon icon={faHouse} />Home</h3>
+      <Navbar />
       <Home />
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,23 @@ import React from "react";
 const Home = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-transparent">
-      <div class="container-fluid" style={{width:'250px', border:'1px solid gray', borderRadius: '10px'}}>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+      <div
+        class="container-fluid"
+        style={{
+          width: "250px",
+          border: "1px solid gray",
+          borderRadius: "10px",
+        }}
+      >
+        <div
+          class="collapse navbar-collapse"
+          id="navbarNavDropdown"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown" style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
           <ul class="navbar-nav">
             <li class="nav-item dropdown">
               <a
@@ -24,10 +28,13 @@ const Home = () => {
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
-               >
+              >
                 Browse Collections
               </a>
-              <ul class="dropdown-menu" style={{width: "250px", margin: "5px -50px" }}>
+              <ul
+                class="dropdown-menu"
+                style={{ width: "250px", margin: "5px -50px" }}
+              >
                 <li>
                   <a class="dropdown-item" href="#">
                     Action
