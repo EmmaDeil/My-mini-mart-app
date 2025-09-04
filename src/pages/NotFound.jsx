ce85ec6e69e23ba339cf9a3a9ca0f899b74cae81
David Emmanuel <eclefzy@gmail.com>
2025-09-04 20:16:09 +0100

    worked on the styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,11 +12,13 @@ const NotFound = () => {
     );
   }, [location.pathname]);
   return (
-  <div className="container text-center mt-5 mb-5">
+  <div className="container-fluid text-center mt-5 mb-5 py-5">
+    <div className="container p-5">
     <h1 className="text-center">404</h1>
     <p className="text-center">Oops! Page Not Found</p>
     <p>The page you are looking for does not exist or has been moved.</p>
     <a href="/" className="btn btn-primary mt-3">Go Home</a>
+    </div>
   </div>
   );
 };
