3ad899055a22e8f51ab7112065c1da817b6a8868
David Emmanuel <eclefzy@gmail.com>
2025-09-04 20:29:26 +0100

    worked on he index.html

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,45 +23,60 @@ const About = () => {
       <div className="container">
         <div className="row our-values">
           <div className="col rad">
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-            <FontAwesomeIcon icon={faShieldAlt} />
-            <h4>Innovation</h4>
-            <p>
-              We embrace change and continuously seek new ways to improve our
-              solutions.
-            </p>
-</SpotlightCard>
+            <SpotlightCard
+              className="custom-spotlight-card"
+              spotlightColor="rgba(0, 229, 255, 0.2)"
+            >
+              <FontAwesomeIcon icon={faShieldAlt} />
+              <h4>Innovation</h4>
+              <p>
+                We embrace change and continuously seek new ways to improve our
+                solutions.
+              </p>
+            </SpotlightCard>
           </div>
           <div className="col rad">
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-            <FontAwesomeIcon icon={faShieldAlt} />
-            <h4>Integrity</h4>
-            <p>We conduct our business with honesty and transparency.</p>
-</SpotlightCard>
+            <SpotlightCard
+              className="custom-spotlight-card"
+              spotlightColor="rgba(0, 229, 255, 0.2)"
+            >
+              <FontAwesomeIcon icon={faShieldAlt} />
+              <h4>Integrity</h4>
+              <p>We conduct our business with honesty and transparency.</p>
+            </SpotlightCard>
           </div>
           <div className="col rad">
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-            <FontAwesomeIcon icon={faShieldAlt} />
-            <h4>Customer Focus</h4>
-            <p>
-              We prioritize our clients' needs and strive to exceed their
-              expectations.
-            </p>
-</SpotlightCard>
+            <SpotlightCard
+              className="custom-spotlight-card"
+              spotlightColor="rgba(0, 229, 255, 0.2)"
+            >
+              <FontAwesomeIcon icon={faShieldAlt} />
+              <h4>Customer Focus</h4>
+              <p>
+                We prioritize our clients' needs and strive to exceed their
+                expectations.
+              </p>
+            </SpotlightCard>
           </div>
           <div className="col rad">
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-            <FontAwesomeIcon icon={faShieldAlt} />
-            <h4>Collaboration</h4>
-            <p>We believe in the power of teamwork and partnerships.</p>
-</SpotlightCard>
+            <SpotlightCard
+              className="custom-spotlight-card"
+              spotlightColor="rgba(0, 229, 255, 0.2)"
+            >
+              <FontAwesomeIcon icon={faShieldAlt} />
+              <h4>Collaboration</h4>
+              <p>We believe in the power of teamwork and partnerships.</p>
+            </SpotlightCard>
           </div>
           <div className="col rad">
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-            <FontAwesomeIcon icon={faShieldAlt} />
-            <h4>Sustainability</h4>
-            <p>We are committed to environmentally responsible practices.</p>
-</SpotlightCard>
+            <SpotlightCard
+              className="custom-spotlight-card"
+              spotlightColor="rgba(0, 229, 255, 0.2)"
+            >
+              <FontAwesomeIcon icon={faShieldAlt} />
+              <h4>Sustainability</h4>
+              <p>We are committed to environmentally responsible practices.</p>
+            </SpotlightCard>
           </div>
         </div>
       </div>
@@ -72,48 +87,56 @@ const About = () => {
         and expertise in facility management, technology, and customer service.
         We work collaboratively to deliver the best solutions for our clients.
       </p>
-      <div className="team-section">
-         <div className="row">
-        <span className="col-6 col-md-4">
-          <img
-            src="../src/assets/man1.png"
-            alt="David Emma"
-            className="teamImg"
-          />
-          <h4>David Emmaunel O.</h4>
-          <p><i>CEO</i></p>
-        </span>
-        <span className="col-6 col-md-4">
-          <img
-            src="../src/assets/man1.png"
-            alt="John Doe"
-            className="teamImg"
-          />
-          <h4>John Doe</h4>
-          <p><i>Facility Manager</i></p>
-        </span>
-        <span className="col-6 col-md-4">
-          <img
-            src="../src/assets/man1.png"
-            alt="Jane Smith"
-            className="teamImg"
-          />
+      <div className="container-fluid team-section">
+        <div className="row">
+          <span className="col">
+            <img
+              src="../src/assets/man1.png"
+              alt="David Emma"
+              className="teamImg"
+            />
+            <h4>David Emmaunel O.</h4>
+            <p>
+              <i>CEO</i>
+            </p>
+          </span>
+          <span className="col">
+            <img
+              src="../src/assets/man1.png"
+              alt="John Doe"
+              className="teamImg"
+            />
+            <h4>John Doe</h4>
+            <p>
+              <i>Facility Manager</i>
+            </p>
+          </span>
+          <span className="col">
+            <img
+              src="../src/assets/man1.png"
+              alt="Jane Smith"
+              className="teamImg"
+            />
 
-          <h4>Jane Smith</h4>
-          <p><i>Technology Specialist</i></p>
-        </span>
-        <span className="col-6 col-md-4">
-          <img
-            src="../src/assets/man1.png"
-            alt="Emily Johnson"
-            className="teamImg"
-          />
-          <h4>Emily Johnson</h4>
-          <p><i>Customer Service Representative</i></p>
-        </span>
+            <h4>Jane Smith</h4>
+            <p>
+              <i>Technology Specialist</i>
+            </p>
+          </span>
+          <span className="col">
+            <img
+              src="../src/assets/man1.png"
+              alt="Emily Johnson"
+              className="teamImg"
+            />
+            <h4>Emily Johnson</h4>
+            <p>
+              <i>Customer Service Representative</i>
+            </p>
+          </span>
+        </div>
       </div>
-         </div>
-{/* <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+      {/* <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
   
 </SpotlightCard> */}
     </div>
