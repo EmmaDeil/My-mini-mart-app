347775b2c7b248965939379f45af656cf212c357
David Emmanuel <eclefzy@gmail.com>
2025-09-14 20:20:51 +0100

    the vite file was edited and the index.html file

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,14 +12,20 @@ const NotFound = () => {
     );
   }, [location.pathname]);
   return (
-  <div className="container-fluid text-center mt-5 mb-5 py-5">
-    <div className="container p-5">
-    <h1 className="text-center">404</h1>
-    <p className="text-center">Oops! Page Not Found</p>
-    <p>The page you are looking for does not exist or has been moved.</p>
-    <a href="/" className="btn btn-primary mt-3">Go Home</a>
+    <div className="container-fluid text-center mt-5 mb-5 py-5">
+      <div className="container p-5">
+        <h1 className="text-center display-1 mb-4">404</h1>
+        <p className="text-center mb-4 text-danger text-xl">
+          Oops! Page Not Found
+        </p>
+        <p className="mb-4">
+          The page you are looking for does not exist or has been moved.
+        </p>
+        <a href="/" className="btn btn-primary mt-3 px-5">
+          Go Home
+        </a>
+      </div>
     </div>
-  </div>
   );
 };
 
