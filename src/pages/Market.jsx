e826a3ae3bbbd35539a97880639217c7957446b8
David Emmanuel <eclefzy@gmail.com>
2025-09-17 22:35:35 +0100

    styled a bit of the productCard

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Market = () => {
+const Market = ({ price }) => {
   return (
     <div>
-      <div className='card text-center p-5 m-5'>
-         <img src="https://via.placeholder.com/150" alt="" />
-         <div className='card-body'>
-            <p>Product Name</p>
-         <div className='card-title'>
-   <p>#20,000</p>
-   <div className='card-text d-flex justify-content-end align-items-end gap-3'>
-      <button>+</button>
-   <p>0</p>
-<button>-</button>
-   </div>
-         </div>
-         </div>
+      <div className="card text-center m-5" style={{width: "18rem"}}>
+        <img src="testimg.jpg" alt="" className="card-img-top" />
+        <div className="card-body">
+          <p className="card-text" style={{fontWeight: "bold", display: "flex", alignItems: "flex-start"}}>Beauty Chocolate Book</p>
+          <div className="card-title" style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+            <h3>#20,000</h3>
+            <div className="card-text d-flex justify-content-end align-items-end gap-3">
+              <button>+</button>
+              <h3>{price}</h3>
+              <button>-</button>
+            </div>
+          </div>
+            <button className="btn btn-primary w-100 mt-3 mb-1">Add to Cart</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Market;
