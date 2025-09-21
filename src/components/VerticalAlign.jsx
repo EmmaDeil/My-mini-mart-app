57fcf13036032249217b9eb177b80917d0ebb6ca
David Emmanuel <eclefzy@gmail.com>
2025-09-21 11:26:35 +0100

    made and edit

--- src/components/VerticalAlign.jsx
+++ src/components/VerticalAlign.jsx
@@ -5,29 +5,29 @@ const VerticalAlign = () => {
     <div className="container text-center" id='vertical-align'>
       <div className="row" style={{ minHeight: '400px' }}>
         <div className="col align-self-start">
-          <div className="p-3 bg-light border rounded" style={{ height: '10rem' }}>
+          <div className="p-3 bg-light border rounded" style={{ height: '10rem', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
             We embrace change and continuously seek new ways to improve our solutions.
           </div>
         </div>
         <div className="col align-self-center">
-          <div className="p-3 bg-primary text-white border rounded" style={{ height: '10rem' }}>
+          <div className="p-3 bg-primary text-white border rounded" style={{ height: '10rem', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
            We conduct our business with honesty and transparency.
           </div>
         </div>
         <div className="col align-self-end">
-          <div className="p-3 bg-success text-white border rounded" style={{ height: '10rem' }}>
+          <div className="p-3 bg-success text-white border rounded" style={{ height: '10rem', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
             We prioritize our clients' needs and strive to exceed their expectations.
           </div>
         </div>
       </div>
       <div className="row" style={{ minHeight: '400px', marginTop: '2rem' }}>
         <div className="col align-self-start">
-          <div className="p-3 bg-light border rounded" style={{ height: '10rem' }}>
+          <div className="p-3 bg-dark text-white border rounded" style={{ height: '10rem', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
            We believe in the power of teamwork and partnerships.
           </div>
         </div>
         <div className="col align-self-center">
-          <div className="p-3 bg-primary text-white border rounded" style={{ height: '10rem' }}>
+          <div className="p-3 bg-info text-white border rounded" style={{ height: '10rem', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
            We are committed to environmentally responsible practices.
           </div>
         </div>
